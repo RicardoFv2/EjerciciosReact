eef4880dc85c7c8c367236677549ef73fd91d61c
Ricardo Fuentes <ricardisimofuentes@hotmail.com>
2023-08-19 19:09:54 -0600

    Contador funcional

--- src/components/routes/contador.jsx
+++ src/components/routes/contador.jsx
@@ -1,18 +1,37 @@
 import React from "react";
 import '../css/contador.css';
 import Navbar from "../navbar";
+import { useState } from "react";
 
 // Crear un Contador Simple:
 // Crea un componente que muestre un contador en la pantalla y tenga botones para incrementar
 // y decrementar el valor del contador.
 
-const contador = () =>{
+const Contador = () =>{
+
+    const [contar, setContar] = useState(0);
+
+    const incrementar = ()=>{
+        setContar(contar + 1);
+    }
+
+    const decrementar = ()=>{
+        setContar(contar - 1);
+    }
+
+
     return(
-        <div>
+        <div className="contador">
             <Navbar/>
             <h1>Contador</h1>
+            <br />
+            
+            <button type="button" onClick={incrementar}>Incrementar</button>
+            <button type="button" onClick={decrementar}>Decrementar</button>
+            <br />
+            <p>Valor: {contar}</p>
         </div>
     )
 };
 
-export default contador
+export default Contador
